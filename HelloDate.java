9b16fe6296115866d10b59a95201d53e73515c67
boslviv <boslviv@gmail.com>
2017-10-14 23:09:56 +0300

    deleted Readme, and same changes in HelloDate

--- HelloDate.java
+++ HelloDate.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class HelloDate {
 	public static void main(String[] args) {
-		System.out.println("hell");
+		System.out.println("Hello naw is:");
 		
 		System.out.println(new Date());
 	}
