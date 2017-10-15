d80c193b1b2d4bf6daad97b4aa09d96c24b2367a
boslviv <boslviv@gmail.com>
2017-10-15 15:15:58 +0300

    My first commit(use "git push" to publish your local commits)

--- src/chapter2/Bool.java
+++ src/chapter2/Bool.java
@@ -1,4 +1,14 @@
 package chapter2;
 
+import java.util.Random;
+
 public class Bool {
+    public static void main(String[] args) {
+        Random rand = new Random(9  ); // не зрозумів чому і як міняється генерування випадкового числа
+        int i = rand.nextInt(100);
+        int j = rand.nextInt(100);
+        System.out.println("i = "+i);
+        System.out.println("j = "+j);
+
+    }
 }
